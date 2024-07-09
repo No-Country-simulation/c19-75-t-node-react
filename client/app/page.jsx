17997a869d7bf280bb581866f5c9c4926d93448a
ignacio <ignaciocarrabs@gmail.com>
2024-07-09 18:37:53 -0300

    header

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -1,9 +1,13 @@
+"use client";
 
+import "./globals.scss";
 
 export default function Home() {
   return (
-    <main>
-      HomePage 
-    </main>
+    <div className="">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid eos iure
+      a eveniet consequatur quaerat quod ad saepe, id sapiente delectus aliquam
+      unde tempore facilis libero, deleniti, excepturi quos eius?
+    </div>
   );
 }
