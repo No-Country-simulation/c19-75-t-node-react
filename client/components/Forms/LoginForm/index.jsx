e98e9ef810a3fd201f70b55b840f9223cf531946
Gianfranco Iacono <iacono.gian13@gmail.com>
2024-07-17 16:21:11 -0300

    add form validation and error handling UI

--- client/components/Forms/LoginForm/index.jsx
+++ client/components/Forms/LoginForm/index.jsx
@@ -1,14 +1,40 @@
+'use client';
 import styles from '../Form.module.scss';
 import Link from 'next/link';
+
+import { loginUserAction } from '@/lib/auth';
+import { useFormState } from 'react-dom';
+const INITIAL_STATE_FORM = {
+  zodErrors: {},
+  message: '',
+  data: {
+    email: '',
+    password: '',
+  },
+};
 
 import Input from '@/components/Forms/Input';
 import ButtonForm from '@/components/Forms/ButtonForm';
 
 const LoginForm = () => {
+  const [formState, formAction] = useFormState(
+    loginUserAction,
+    INITIAL_STATE_FORM
+  );
   return (
-    <form action="" className={styles.form} name="loginForm">
-      <Input type="Email" name="email" placeholder="Email" />
-      <Input type="password" name="password" placeholder="Contraseña" />
+    <form action={formAction} className={styles.form} name="loginForm">
+      <Input
+        type="Email"
+        name="email"
+        placeholder="Email"
+        error={formState?.zodErrors?.email}
+      />
+      <Input
+        type="password"
+        name="password"
+        placeholder="Contraseña"
+        error={formState?.zodErrors?.password}
+      />
       <p className={`${styles.text} ${styles.text__right}`}>
         <Link href="/reset-password" className={styles.link}>
           ¿Olvidaste tu contraseña?
