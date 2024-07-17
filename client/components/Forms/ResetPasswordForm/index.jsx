2e9bd360ad7b360cfddeed168123f5ef07b08f37
Gianfranco Iacono <iacono.gian13@gmail.com>
2024-07-17 15:45:31 -0300

    Remove unused code and update ResetPasswordForm layout

--- client/components/Forms/ResetPasswordForm/index.jsx
+++ client/components/Forms/ResetPasswordForm/index.jsx
@@ -1,5 +1,4 @@
 import styles from '../Form.module.scss';
-import Link from 'next/link';
 
 import Input from '@/components/Forms/Input';
 import ButtonForm from '@/components/Forms/ButtonForm';
@@ -12,9 +11,8 @@ const ResetPasswordForm = () => {
         para restablecer tu contraseña.
       </p>
       <Input type="text" name="email" placeholder="Email" />
-      <div className={styles.footer}>
-        <ButtonForm type="submit">Restablecer Contraseña</ButtonForm>
-      </div>
+
+      <ButtonForm type="submit">Restablecer Contraseña</ButtonForm>
     </form>
   );
 };
