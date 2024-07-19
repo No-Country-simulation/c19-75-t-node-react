b5f48596036114f13976090270292c36460a86fa
Gianfranco Iacono <iacono.gian13@gmail.com>
2024-07-19 03:01:59 -0300

    remove duplicate import statement for global styles

--- client/app/layout.jsx
+++ client/app/layout.jsx
@@ -3,10 +3,6 @@ import Footer from '@/components/Footer';
 
 import '@/styles/globals.scss';
 
-import '@/styles/globals.scss';
-
-import '@/styles/globals.scss';
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
