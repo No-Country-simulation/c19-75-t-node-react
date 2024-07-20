d578b3a6b6582c29cc3aa2047b5d92fcb2438972
Gianfranco Iacono <iacono.gian13@gmail.com>
2024-07-20 18:58:57 -0300

    update styles for authentication pages

--- client/containers/auth-page/singup-page/index.jsx
+++ client/containers/auth-page/singup-page/index.jsx
@@ -6,21 +6,23 @@ import Link from 'next/link';
 const SingupPage = () => {
   return (
     <section className={styles.section}>
-      <div className={styles.header}>
-        <h1 className={styles.title}>Crear una Cuenta</h1>
-        <div className={`${styles.divider}`}></div>
-      </div>
-      <div className={styles.body}>
-        <SingupForm />
-      </div>
-      <div className={styles.footer}>
-        {/* <div className={`${styles.divider} ${styles.divider__small}`}></div> */}
-        <p className={styles.text}>
-          ¿Ya tienes una cuenta?{' '}
-          <Link href="/login" className={styles.link}>
-            Iniciar Sesion
-          </Link>
-        </p>
+      <div className={styles.container}>
+        <div className={styles.header}>
+          <h1 className={styles.title}>Crear una Cuenta</h1>
+          <div className={`${styles.divider}`}></div>
+        </div>
+        <div className={styles.body}>
+          <SingupForm />
+        </div>
+        <div className={styles.footer}>
+          {/* <div className={`${styles.divider} ${styles.divider__small}`}></div> */}
+          <p className={styles.text}>
+            ¿Ya tienes una cuenta?{' '}
+            <Link href="/login" className={styles.link}>
+              Iniciar Sesion
+            </Link>
+          </p>
+        </div>
       </div>
     </section>
   );
