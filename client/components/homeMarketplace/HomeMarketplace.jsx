2e58c37812fad2f5159d3e52d66c1d25e733354a
ignacio <ignaciocarrabs@gmail.com>
2024-07-22 16:10:29 -0300

    solucion error marketplace

--- client/components/homeMarketplace/HomeMarketplace.jsx
+++ client/components/homeMarketplace/HomeMarketplace.jsx
@@ -5,51 +5,71 @@ import { FcElectricity } from "react-icons/fc";
 import { MdOutlinePlumbing, MdConstruction, MdCarpenter } from "react-icons/md";
 import { FaFire, FaPaintRoller } from "react-icons/fa";
 import { GiGardeningShears, GiBlacksmith } from "react-icons/gi";
-
+import Link from "next/link";
 
 const homeMarketplace = () => {
   return (
     <div className={styles.homemarketplace}>
       <div className={styles.container}>
         <h1>Marketplace para trabajadores.</h1>
-        <h2>Explora las distintas categorías y encontrá el laburo ideal para vos.</h2>
+        <h2>
+          Explora las distintas categorías y encontrá el laburo ideal para vos.
+        </h2>
         <div className={styles.categories}>
-          <div className={styles.button}>
-            <h3 className={styles.title}>Mantenimiento</h3>
-            <GrVmMaintenance className={styles.icon}/>
-          </div>
-          <div className={styles.button}>
-            <h3 className={styles.title}>Electricista</h3>
-            <FcElectricity className={styles.icon}/>
-          </div>
-          <div className={styles.button}>
-            <h3 className={styles.title}>Plomería</h3>
-            <MdOutlinePlumbing className={styles.icon}/>
-          </div>
-          <div className={styles.button}>
-            <h3 className={styles.title}>Gasista</h3>
-            <FaFire className={styles.icon}/>
-          </div>
-          <div className={styles.button}>
-            <h3 className={styles.title}>Albañilería</h3>
-            <MdConstruction className={styles.icon}/>
-          </div>
-          <div className={styles.button}>
-            <h3 className={styles.title}>Jardinería</h3>
-            <GiGardeningShears className={styles.icon}/>
-          </div>
-          <div className={styles.button}>
-            <h3 className={styles.title}>Pinturería</h3>
-            <FaPaintRoller className={styles.icon}/>
-          </div>
-          <div className={styles.button}>
-            <h3 className={styles.title}>Carpintería</h3>
-            <MdCarpenter className={styles.icon}/>
-          </div>
-          <div className={styles.button}>
-            <h3 className={styles.title}>Herrería</h3>
-            <GiBlacksmith className={styles.icon}/>
-          </div>
+          <Link href="/" legacyBehavior>
+            <a className={styles.button}>
+              <h3 className={styles.title}>Mantenimiento</h3>
+              <GrVmMaintenance className={styles.icon} />
+            </a>
+          </Link>
+          <Link href="/" legacyBehavior>
+            <a className={styles.button}>
+              <h3 className={styles.title}>Electricista</h3>
+              <FcElectricity className={styles.icon} />
+            </a>
+          </Link>
+          <Link href="/" legacyBehavior>
+            <a className={styles.button}>
+              <h3 className={styles.title}>Plomería</h3>
+              <MdOutlinePlumbing className={styles.icon} />
+            </a>
+          </Link>
+          <Link href="/" legacyBehavior>
+            <a className={styles.button}>
+              <h3 className={styles.title}>Gasista</h3>
+              <FaFire className={styles.icon} />
+            </a>
+          </Link>
+          <Link href="/" legacyBehavior>
+            <a className={styles.button}>
+              <h3 className={styles.title}>Albañilería</h3>
+              <MdConstruction className={styles.icon} />
+            </a>
+          </Link>
+          <Link href="/" legacyBehavior>
+            <a className={styles.button}>
+              <h3 className={styles.title}>Jardinería</h3>
+              <GiGardeningShears className={styles.icon} />
+            </a>
+          </Link>
+          <Link href="/" legacyBehavior>
+            <a className={styles.button}>
+              <h3 className={styles.title}>Pinturería</h3>
+              <FaPaintRoller className={styles.icon} />
+            </a>
+          </Link>
+          <Link href="/" legacyBehavior>
+            <a className={styles.button}>
+              <h3 className={styles.title}>Carpintería</h3>
+              <MdCarpenter className={styles.icon} />
+            </a>
+          </Link>
+          <Link href="/" legacyBehavior>
+            <a className={styles.button}>
+              <h3 className={styles.title}>Herrería</h3>
+              <GiBlacksmith className={styles.icon} />
+            </a>
+          </Link>
         </div>
       </div>
     </div>
