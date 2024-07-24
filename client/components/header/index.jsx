23dc1564de06cc15679a8227b29a4a7dfbc540ce
Gianfranco Iacono <iacono.gian13@gmail.com>
2024-07-24 03:50:13 -0300

    add: links to pages and some styles

--- client/components/header/index.jsx
+++ client/components/header/index.jsx
@@ -6,155 +6,154 @@ import { useState, useEffect } from 'react';
 import { usePathname } from 'next/navigation';
 
 const Header = () => {
-  const [active, setActive] = useState(true);
-  const [open, setOpen] = useState(false);
+    const [active, setActive] = useState(true);
+    const [open, setOpen] = useState(false);
 
-  const pathname = usePathname();
+    const pathname = usePathname();
 
-  const isActive = () => {
-    window.scrollY > 0 ? setActive(true) : setActive(false);
-  };
-
-  useEffect(() => {
-    window.addEventListener('scroll', isActive);
-
-    return () => {
-      window.removeEventListener('scroll', isActive);
+    const isActive = () => {
+        window.scrollY > 0 ? setActive(true) : setActive(false);
     };
-  }, []);
 
-  // const user = {
-  //   id: 1,
-  //   userType: 'cliente',
-  //   name: 'Nacho',
-  // };
-  const user = null;
+    useEffect(() => {
+        window.addEventListener('scroll', isActive);
 
-  const SignupRender = () => {
-    return (
-      <>
-        {pathname !== '/signup' && (
-          <>
-            <div className={styles.link}>
-              <Link href="/signup?role=trabajador">
-                <h4>Quiero trabajar</h4>
-              </Link>
-            </div>
-            <div className={styles.link}>
-              <Link href="/signup?role=cliente">
-                <h4>Quiero contratar</h4>
-              </Link>
-            </div>
-          </>
-        )}
-        {pathname !== '/login' && (
-          <div className={styles.link}>
-            <Link href="/login">
-              <h4>Iniciar Sesión</h4>
-            </Link>
-          </div>
-        )}
-      </>
-    );
-  };
+        return () => {
+            window.removeEventListener('scroll', isActive);
+        };
+    }, []);
 
-  const OptionsRender = (isTrabajador) => {
-    return (
-      <div className={styles.options}>
-        {isTrabajador && (
-          <>
-            <Link href="/mislaburos" className={styles.link}>
-              Laburos
-            </Link>
-            <Link href="/add" className={styles.link}>
-              Agregar nuevo laburo
-            </Link>
-            <Link href="/orders" className={styles.link}>
-              Encargos
-            </Link>
-          </>
-        )}
-        <Link href="/messages" className={styles.link}>
-          Mensajes
-        </Link>
-        <Link href="/" className={styles.link}>
-          Cerrar sesión
-        </Link>
-      </div>
-    );
-  };
+    const user = {
+        id: 1,
+        userType: 'cliente',
+        name: 'Nacho',
+    };
+    // const user = null;
 
-  return (
-    <header
-      className={`${styles.navbar} ${
-        active || pathname !== '/' ? styles.active : ''
-      }`}
-    >
-      <div className={styles.container}>
-        <div className={styles.logo}>
-          <Link href="/" className={styles.link}>
-            <img src="" />
-            <span className={styles.text}>
-              <span className={styles.labur}>Labur</span>app.
-            </span>
-          </Link>
-        </div>
-        <div className={styles.links}>
-          {!user ? (
-            <SignupRender />
-          ) : (
+    const SignupRender = () => {
+        return (
             <>
-              {user?.userType === 'cliente' && <span>Ofrecer mis changas</span>}
-              <div className={styles.user} onClick={() => setOpen(!open)}>
-                <img src="" alt="" />
-                <span>{user?.name}</span>
-                {open && (
-                  <OptionsRender
-                    isTrabajador={user?.userType === 'trabajador'}
-                  />
+                {pathname !== '/signup' && (
+                    <>
+                        <div className={styles.link}>
+                            <Link href="/signup?role=trabajador">
+                                <h4>Quiero trabajar</h4>
+                            </Link>
+                        </div>
+                        <div className={styles.link}>
+                            <Link href="/signup?role=cliente">
+                                <h4>Quiero contratar</h4>
+                            </Link>
+                        </div>
+                    </>
                 )}
-              </div>
+                {pathname !== '/login' && (
+                    <div className={styles.link}>
+                        <Link href="/login">
+                            <h4>Iniciar Sesión</h4>
+                        </Link>
+                    </div>
+                )}
             </>
-          )}
-        </div>
-      </div>
-      {active && pathname === '/' && (
-        <>
-          <hr />
-          <div className={styles.menu}>
-            <Link href="/" className={styles.link}>
-              Mantenimiento
-            </Link>
-            <Link href="/" className={styles.link}>
-              Albañiería
-            </Link>
-            <Link href="/" className={styles.link}>
-              Plomería
-            </Link>
-            <Link href="/" className={styles.link}>
-              Gasistas
-            </Link>
-            <Link href="/" className={styles.link}>
-              Electricistas
-            </Link>
-            <Link href="/" className={styles.link}>
-              Jardinería
-            </Link>
+        );
+    };
 
-            <Link href="/" className={styles.link}>
-              Pinturería
-            </Link>
-            <Link href="/" className={styles.link}>
-              Carpintería
-            </Link>
-            <Link href="/" className={styles.link}>
-              Herrería
-            </Link>
-          </div>
-        </>
-      )}
-    </header>
-  );
+    const OptionsRender = ({ isTrabajador }) => {
+        return (
+            <div className={styles.options}>
+                <Link href={`/${user.name}`} className={styles.link}>
+                    Perfil
+                </Link>
+                {isTrabajador && (
+                    <>
+                        <Link href={`/${user.name}?opcion=laburos`} className={styles.link}>
+                            Laburos
+                        </Link>
+                        <Link href={`/${user.name}?opcion=ordenes`} className={styles.link}>
+                            Ordenes
+                        </Link>
+                        <Link href={`/${user.name}?opcion=postulaciones`} className={styles.link}>
+                            Postulaciones
+                        </Link>
+                    </>
+                )}
+                {!isTrabajador && (
+                    <Link href={`/${user.name}?opcion=changas`} className={styles.link}>
+                        Changas
+                    </Link>
+                )}
+                <Link href="/" className={`${styles.link} ${styles.linkMenu}`}>
+                    Cerrar sesión
+                </Link>
+            </div>
+        );
+    };
+
+    return (
+        <header className={`${styles.navbar} ${active || pathname !== '/' ? styles.active : ''}`}>
+            <div className={styles.container}>
+                <div className={styles.logo}>
+                    <Link href="/" className={styles.link}>
+                        <img src="" />
+                        <span className={styles.text}>
+                            <span className={styles.labur}>Labur</span>app.
+                        </span>
+                    </Link>
+                </div>
+                <div className={styles.links}>
+                    {!user ? (
+                        <SignupRender />
+                    ) : (
+                        <>
+                            {user?.userType === 'cliente' && <span>Ofrecer mis changas</span>}
+                            <div className={styles.user}>
+                                <div onClick={() => setOpen(!open)}>
+                                    <img src="" alt="" />
+                                    <span className={styles.link}>{user?.name}</span>
+                                </div>
+                                {open && <OptionsRender isTrabajador={user?.userType === 'trabajador'} />}
+                            </div>
+                        </>
+                    )}
+                </div>
+            </div>
+            {active && pathname === '/' && (
+                <>
+                    <hr />
+                    <div className={styles.menu}>
+                        <Link href="/" className={styles.link}>
+                            Mantenimiento
+                        </Link>
+                        <Link href="/" className={styles.link}>
+                            Albañiería
+                        </Link>
+                        <Link href="/" className={styles.link}>
+                            Plomería
+                        </Link>
+                        <Link href="/" className={styles.link}>
+                            Gasistas
+                        </Link>
+                        <Link href="/" className={styles.link}>
+                            Electricistas
+                        </Link>
+                        <Link href="/" className={styles.link}>
+                            Jardinería
+                        </Link>
+
+                        <Link href="/" className={styles.link}>
+                            Pinturería
+                        </Link>
+                        <Link href="/" className={styles.link}>
+                            Carpintería
+                        </Link>
+                        <Link href="/" className={styles.link}>
+                            Herrería
+                        </Link>
+                    </div>
+                </>
+            )}
+        </header>
+    );
 };
 
 export default Header;
