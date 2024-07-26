4b73673d554ddd3a343727799c447e2c71871d15
Gianfranco Iacono <iacono.gian13@gmail.com>
2024-07-26 05:45:20 -0300

    lazy commit

--- client/app/layout.jsx
+++ client/app/layout.jsx
@@ -1,19 +1,19 @@
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
-import { HeaderProvider } from '@/context/HeaderContext';
+import { SessionProvider } from '@/context/SessionContext';
 
 import '@/styles/globals.scss';
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>
-        <HeaderProvider>
-          <Header />
-          <main>{children}</main>
-        </HeaderProvider>
-        <Footer />
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body>
+                <SessionProvider>
+                    <Header />
+                    <main>{children}</main>
+                </SessionProvider>
+                <Footer />
+            </body>
+        </html>
+    );
 }
--- client/components/Forms/LoginForm/index.jsx
+++ client/components/Forms/LoginForm/index.jsx
@@ -5,12 +5,12 @@ import Link from 'next/link';
 import { login } from '@/actions/auth';
 import { useFormState } from 'react-dom';
 const INITIAL_STATE_FORM = {
-  zodErrors: {},
-  message: '',
-  data: {
-    email: '',
-    password: '',
-  },
+    zodErrors: {},
+    message: '',
+    data: {
+        email: '',
+        password: '',
+    },
 };
 
 import Input from '@/components/Forms/Input';
@@ -18,57 +18,57 @@ import ButtonForm from '@/components/Forms/ButtonForm';
 import ZodErrors from '../Custom';
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import { useHeaderContext } from '@/context/HeaderContext';
+import { useSessionContext } from '@/context/SessionContext';
 
 const LoginForm = () => {
-  const router = useRouter();
-  const { setSessionActive } = useHeaderContext();
-  const [formState, formAction, pending] = useFormState(login, INITIAL_STATE_FORM);
+    const router = useRouter();
+    const { setSessionActive } = useSessionContext();
+    const [formState, formAction, pending] = useFormState(login, INITIAL_STATE_FORM);
 
-  useEffect(() => {
-    if (formState?.success) {
-      setSessionActive(true);
-      router.push('/');
-    }
-  }, [formState?.success]);
+    useEffect(() => {
+        if (formState?.success) {
+            setSessionActive(true);
+            router.push('/');
+        }
+    }, [formState?.success]);
 
-  return (
-    <form action={formAction} className={styles.form} name="loginForm">
-      {!formState?.success && (
-        <ZodErrors
-          error={formState?.errors?.principal}
-          type="principal"
-          success={formState?.success}
-        />
-      )}
-      <div className={styles.campo}>
-        <Input
-          type="Email"
-          name="email"
-          placeholder="Email"
-          error={formState?.errors?.email}
-          defaultValue={'profesional@gmail.com'} // FIXME: Delete this line
-        />
-      </div>
-      <div className={styles.campo}>
-        <Input
-          type="password"
-          name="password"
-          placeholder="Contraseña"
-          error={formState?.errors?.password}
-        />
-      </div>
-      <p className={`${styles.text} ${styles.text__right}`}>
-        <Link href="/reset-password" className={styles.link}>
-          ¿Olvidaste tu contraseña?
-        </Link>
-      </p>
+    return (
+        <form action={formAction} className={styles.form} name="loginForm">
+            {!formState?.success && (
+                <ZodErrors
+                    error={formState?.errors?.principal}
+                    type="principal"
+                    success={formState?.success}
+                />
+            )}
+            <div className={styles.campo}>
+                <Input
+                    type="Email"
+                    name="email"
+                    placeholder="Email"
+                    error={formState?.errors?.email}
+                    defaultValue={'profesional@gmail.com'} // FIXME: Delete this line
+                />
+            </div>
+            <div className={styles.campo}>
+                <Input
+                    type="password"
+                    name="password"
+                    placeholder="Contraseña"
+                    error={formState?.errors?.password}
+                />
+            </div>
+            <p className={`${styles.text} ${styles.text__right}`}>
+                <Link href="/reset-password" className={styles.link}>
+                    ¿Olvidaste tu contraseña?
+                </Link>
+            </p>
 
-      <ButtonForm type="submit" pending={pending} textPending={'Iniciando...'}>
-        Iniciar Sesion
-      </ButtonForm>
-    </form>
-  );
+            <ButtonForm type="submit" pending={pending} textPending={'Iniciando...'}>
+                Iniciar Sesion
+            </ButtonForm>
+        </form>
+    );
 };
 
 export default LoginForm;
