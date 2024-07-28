b4396d3722a21f4830cf8ea6f3f6db8d6b06e5c9
Gianfranco Iacono <iacono.gian13@gmail.com>
2024-07-28 07:21:28 -0300

    feat: Add new icons for dropdown and star functionality

--- client/components/Icons/index.jsx
+++ client/components/Icons/index.jsx
@@ -1,34 +1,17 @@
 import style from './Icons.module.scss';
-import {
-  FaArrowLeft,
-  FaBookmark,
-  FaFilter,
-  FaSignOutAlt,
-  FaLinkedin,
-} from 'react-icons/fa'; // Importar desde 'fa'
+import { FaArrowLeft, FaBookmark, FaFilter, FaSignOutAlt, FaLinkedin, FaStar } from 'react-icons/fa'; // Importar desde 'fa'
 import { RiInstagramFill } from 'react-icons/ri'; // Importar desde 'ri'
 import { BsTwitterX } from 'react-icons/bs'; // Importar desde 'bs'
+import { IoMdArrowDropdown } from 'react-icons/io';
 
-const Bookmark = ({ otherStyles }) => (
-  <FaBookmark className={`${style.icon} ${otherStyles}`} />
-);
-const Filter = ({ otherStyles }) => (
-  <FaFilter className={`${style.icon} ${otherStyles}`} />
-);
-const ArrowLeft = ({ otherStyles }) => (
-  <FaArrowLeft className={`${style.icon} ${otherStyles}`} />
-);
-const SignOut = ({ otherStyles }) => (
-  <FaSignOutAlt className={`${style.icon} ${otherStyles}`} />
-);
-const Instagram = ({ otherStyles }) => (
-  <RiInstagramFill className={`${style.icon} ${otherStyles}`} />
-);
-const Twitter = ({ otherStyles }) => (
-  <BsTwitterX className={`${style.icon} ${otherStyles}`} />
-);
-const Linkedin = ({ otherStyles }) => (
-  <FaLinkedin className={`${style.icon} ${otherStyles}`} />
-);
+const Bookmark = ({ otherStyles }) => <FaBookmark className={`${style.icon} ${otherStyles}`} />;
+const Filter = ({ otherStyles }) => <FaFilter className={`${style.icon} ${otherStyles}`} />;
+const ArrowLeft = ({ otherStyles }) => <FaArrowLeft className={`${style.icon} ${otherStyles}`} />;
+const SignOut = ({ otherStyles }) => <FaSignOutAlt className={`${style.icon} ${otherStyles}`} />;
+const Instagram = ({ otherStyles }) => <RiInstagramFill className={`${style.icon} ${otherStyles}`} />;
+const Twitter = ({ otherStyles }) => <BsTwitterX className={`${style.icon} ${otherStyles}`} />;
+const Linkedin = ({ otherStyles }) => <FaLinkedin className={`${style.icon} ${otherStyles}`} />;
+const ArrowDropdown = ({ otherStyles }) => <IoMdArrowDropdown className={`${style.icon} ${otherStyles}`} />;
+const Star = ({ otherStyles }) => <FaStar className={`${style.icon} ${otherStyles}`} />;
 
-export { Bookmark, Filter, ArrowLeft, SignOut, Instagram, Twitter, Linkedin };
+export { Bookmark, Filter, ArrowLeft, SignOut, Instagram, Twitter, Linkedin, ArrowDropdown, Star };
