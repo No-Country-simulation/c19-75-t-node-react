89043a84792655eb5ed8ed1e5ef1a43bb8334429
Gianfranco Iacono <iacono.gian13@gmail.com>
2024-07-30 02:04:58 -0300

    feat: add new icons for maintenance, electrician, plumbing, gas, construction, gardening, painting, carpentry, and blacksmith

--- client/components/Icons/index.jsx
+++ client/components/Icons/index.jsx
@@ -1,34 +1,51 @@
 import style from './Icons.module.scss';
 import {
-  FaArrowLeft,
-  FaBookmark,
-  FaFilter,
-  FaSignOutAlt,
-  FaLinkedin,
+    FaArrowLeft,
+    FaBookmark,
+    FaFilter,
+    FaSignOutAlt,
+    FaLinkedin,
+    FaFire,
+    FaPaintRoller,
 } from 'react-icons/fa'; // Importar desde 'fa'
 import { RiInstagramFill } from 'react-icons/ri'; // Importar desde 'ri'
 import { BsTwitterX } from 'react-icons/bs'; // Importar desde 'bs'
+import { GrVmMaintenance } from 'react-icons/gr';
+import { MdOutlinePlumbing, MdConstruction, MdCarpenter, MdElectricBolt } from 'react-icons/md';
+import { GiGardeningShears, GiBlacksmith } from 'react-icons/gi';
 
-const Bookmark = ({ otherStyles }) => (
-  <FaBookmark className={`${style.icon} ${otherStyles}`} />
-);
-const Filter = ({ otherStyles }) => (
-  <FaFilter className={`${style.icon} ${otherStyles}`} />
-);
-const ArrowLeft = ({ otherStyles }) => (
-  <FaArrowLeft className={`${style.icon} ${otherStyles}`} />
-);
-const SignOut = ({ otherStyles }) => (
-  <FaSignOutAlt className={`${style.icon} ${otherStyles}`} />
-);
-const Instagram = ({ otherStyles }) => (
-  <RiInstagramFill className={`${style.icon} ${otherStyles}`} />
-);
-const Twitter = ({ otherStyles }) => (
-  <BsTwitterX className={`${style.icon} ${otherStyles}`} />
-);
-const Linkedin = ({ otherStyles }) => (
-  <FaLinkedin className={`${style.icon} ${otherStyles}`} />
-);
+const Bookmark = ({ otherStyles }) => <FaBookmark className={`${style.icon} ${otherStyles}`} />;
+const Filter = ({ otherStyles }) => <FaFilter className={`${style.icon} ${otherStyles}`} />;
+const ArrowLeft = ({ otherStyles }) => <FaArrowLeft className={`${style.icon} ${otherStyles}`} />;
+const SignOut = ({ otherStyles }) => <FaSignOutAlt className={`${style.icon} ${otherStyles}`} />;
+const Instagram = ({ otherStyles }) => <RiInstagramFill className={`${style.icon} ${otherStyles}`} />;
+const Twitter = ({ otherStyles }) => <BsTwitterX className={`${style.icon} ${otherStyles}`} />;
+const Linkedin = ({ otherStyles }) => <FaLinkedin className={`${style.icon} ${otherStyles}`} />;
+const Mantenimiento = ({ otherStyles }) => <GrVmMaintenance className={`${style.icon} ${otherStyles}`} />;
+const Electricista = ({ otherStyles }) => <MdElectricBolt className={`${style.icon} ${otherStyles}`} />;
+const Plomeria = ({ otherStyles }) => <MdOutlinePlumbing className={`${style.icon} ${otherStyles}`} />;
+const Gasista = ({ otherStyles }) => <FaFire className={`${style.icon} ${otherStyles}`} />;
+const Albanileria = ({ otherStyles }) => <MdConstruction className={`${style.icon} ${otherStyles}`} />;
+const Jardineria = ({ otherStyles }) => <GiGardeningShears className={`${style.icon} ${otherStyles}`} />;
+const Pintureria = ({ otherStyles }) => <FaPaintRoller className={`${style.icon} ${otherStyles}`} />;
+const Carpinteria = ({ otherStyles }) => <MdCarpenter className={`${style.icon} ${otherStyles}`} />;
+const Herreria = ({ otherStyles }) => <GiBlacksmith className={`${style.icon} ${otherStyles}`} />;
 
-export { Bookmark, Filter, ArrowLeft, SignOut, Instagram, Twitter, Linkedin };
+export {
+    Bookmark,
+    Filter,
+    ArrowLeft,
+    SignOut,
+    Instagram,
+    Twitter,
+    Linkedin,
+    Mantenimiento,
+    Electricista,
+    Plomeria,
+    Gasista,
+    Albanileria,
+    Jardineria,
+    Pintureria,
+    Carpinteria,
+    Herreria,
+};
