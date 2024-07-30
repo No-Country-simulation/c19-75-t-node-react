c7dbba97a4cd43b74ef257ae8f2a59c0afdf22a9
Gianfranco Iacono <iacono.gian13@gmail.com>
2024-07-30 01:22:19 -0300

    add categories on types file

---
+++ client/types/types.js
@@ -0,0 +1,10 @@
+export const CATEGORIES = [
+    { id: 1, name: 'Pinturería', url: 'pintureria' },
+    { id: 2, name: 'Herrería', url: 'herreria' },
+    { id: 3, name: 'Plomería', url: 'plomeria' },
+    { id: 4, name: 'Electricidad', url: 'electricidad' },
+    { id: 5, name: 'Jardinería', url: 'jardineria' },
+    { id: 6, name: 'Mantenimiento', url: 'mantenimiento' },
+    { id: 7, name: 'Albañilería', url: 'albanileria' },
+    { id: 8, name: 'Carpintería', url: 'carpinteria' },
+];
