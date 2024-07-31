a90b8c5f2f198cdf75b37aa3f64a0cf4425274e7
Gianfranco Iacono <iacono.gian13@gmail.com>
2024-07-31 01:59:43 -0300

    add `return` to responses

--- api/controllers/create-user.controller.js
+++ api/controllers/create-user.controller.js
@@ -8,14 +8,28 @@ const sql = require('mssql');
  * @returns {object} - Response object with user data or error message
  */
 const createUser = async (req, res) => {
-    const { nombre, apellido, ciudad, cp, provincia, tel, mail, pass, esprofesional, categorias, barrio } = req.body;
+    const { nombre, apellido, ciudad, cp, provincia, tel, mail, pass, esprofesional, categorias, barrio } =
+        req.body;
 
-    if (!nombre || !apellido || !ciudad || !cp || !provincia || !tel || !mail || !pass || !barrio || esprofesional === undefined) {
-        return res.status(400).json({ error: 'Por favor, ingresar nombre, apellido, ciudad, provincia, cp, tel, mail, barrios, pass y esprofesional' });
+    if (
+        !nombre ||
+        !apellido ||
+        !ciudad ||
+        !cp ||
+        !provincia ||
+        !tel ||
+        !mail ||
+        !pass ||
+        !barrio ||
+        esprofesional === undefined
+    ) {
+        return res.status(400).json({
+            error: 'Por favor, ingresar nombre, apellido, ciudad, provincia, cp, tel, mail, barrios, pass y esprofesional',
+        });
     }
 
     const pool = await connectDB();
-    // Iniciar una transacción para que ambas acciones se completen juntas (ya que si el usuario es profesional, 
+    // Iniciar una transacción para que ambas acciones se completen juntas (ya que si el usuario es profesional,
     //tambien se crea el registro en usuarios), si una falla, se revierte todo.
     const transaction = new sql.Transaction(pool);
 
@@ -23,7 +37,8 @@ const createUser = async (req, res) => {
         await transaction.begin();
 
         // Crear nuevo usuario
-        const usuarioResult = await transaction.request()
+        const usuarioResult = await transaction
+            .request()
             .input('nombre', sql.NVarChar, nombre)
             .input('apellido', sql.NVarChar, apellido)
             .input('ciudad', sql.NVarChar, ciudad)
@@ -33,8 +48,7 @@ const createUser = async (req, res) => {
             .input('tel', sql.NVarChar, tel)
             .input('mail', sql.NVarChar, mail)
             .input('pass', sql.NVarChar, pass)
-            .input('esprofesional', sql.Bit, esprofesional)
-            .query(`
+            .input('esprofesional', sql.Bit, esprofesional).query(`
                 INSERT INTO usuarios (
                     nombre, 
                     apellido, 
@@ -68,7 +82,8 @@ const createUser = async (req, res) => {
 
         // Crear una entrada en la tabla profesionales si el usuario es profesional
         if (esprofesional) {
-            const profesionalResult = await transaction.request()
+            const profesionalResult = await transaction
+                .request()
                 .input('usuario_id', sql.Int, usuarioId)
                 .query('INSERT INTO profesionales (usuario_id) OUTPUT INSERTED.id VALUES (@usuario_id)');
 
@@ -76,10 +91,13 @@ const createUser = async (req, res) => {
 
             if (categorias && categorias.length > 0) {
                 for (const categoria of categorias) {
-                    await transaction.request()
+                    await transaction
+                        .request()
                         .input('profesional_id', sql.Int, profesionalId)
                         .input('categoria_id', sql.Int, categoria)
-                        .query('INSERT INTO profesionalCategorias (profesionalID, categoriaID) VALUES (@profesional_id, @categoria_id)');
+                        .query(
+                            'INSERT INTO profesionalCategorias (profesionalID, categoriaID) VALUES (@profesional_id, @categoria_id)'
+                        );
                 }
             }
         }
@@ -87,7 +105,7 @@ const createUser = async (req, res) => {
         // Confirmar la transacción
         await transaction.commit();
 
-        res.status(201).json({ message: 'Usuario creado exitosamente', id: usuarioId });
+        return res.status(201).json({ message: 'Usuario creado exitosamente', id: usuarioId });
     } catch (err) {
         console.error('Error al crear el usuario:', err);
 
@@ -98,11 +116,10 @@ const createUser = async (req, res) => {
             console.error('Error al deshacer la transacción:', rollbackErr);
         }
 
-        res.status(500).json({ error: 'Error al crear el usuario' });
+        return res.status(500).json({ error: 'Error al crear el usuario' });
     }
-}
-
+};
 
 module.exports = {
-    createUser
+    createUser,
 };
