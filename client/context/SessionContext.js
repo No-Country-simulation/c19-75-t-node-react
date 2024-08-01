e9ab30f57c14d2ecdff22e6083955bec713b2535
Gianfranco Iacono <iacono.gian13@gmail.com>
2024-08-01 07:17:09 -0300

    update SessionProvider to include loggedUserId and isUserWatchingOwnPage in SessionContext

--- client/context/SessionContext.js
+++ client/context/SessionContext.js
@@ -6,27 +6,41 @@ import { userSession } from '@/actions/actions';
 const SessionContext = createContext();
 
 export const SessionProvider = ({ children }) => {
-    const [userSessionData, setUserSessionData] = useState(null);
-    const [sessionActive, setSessionActive] = useState(false);
+  const [userSessionData, setUserSessionData] = useState(null);
+  const [sessionActive, setSessionActive] = useState(false);
+  const [loggedUserId, setLoggedUserId] = useState(null);
 
-    useEffect(() => {
-        const fetchSession = async () => {
-            const sessionData = await userSession(); // Obtiene data del usuario (id, nombre, esprofesional) desde cookie
-            if (sessionData) {
-                setSessionActive(true);
-            }
-            setUserSessionData(sessionData);
-        };
-        fetchSession();
-    }, [sessionActive]);
+  useEffect(() => {
+    const fetchSession = async () => {
+      const sessionData = await userSession(); // Obtiene data del usuario (id, nombre, esprofesional) desde cookie
+      if (sessionData) {
+        setSessionActive(true);
+        setLoggedUserId(sessionData.userId);
+      }
+      setUserSessionData(sessionData);
+    };
+    fetchSession();
+  }, [sessionActive]);
 
-    return (
-        <SessionContext.Provider
-            value={{ userSessionData, setUserSessionData, setSessionActive, sessionActive }}
-        >
-            {children}
-        </SessionContext.Provider>
-    );
+  const isUserWatchingOwnPage = (profileUserId) => {
+    if (!userSessionData) return false;
+    return loggedUserId === Number(profileUserId);
+  };
+
+  return (
+    <SessionContext.Provider
+      value={{
+        userSessionData,
+        setUserSessionData,
+        setSessionActive,
+        sessionActive,
+        isUserWatchingOwnPage,
+        loggedUserId,
+      }}
+    >
+      {children}
+    </SessionContext.Provider>
+  );
 };
 
 export const useSessionContext = () => useContext(SessionContext);
